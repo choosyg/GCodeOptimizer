762d17372147a4f197159c5d4c28dc37c2b2ce98
choosyg <stefan@schiffler-bremen.de>
2023-03-29 09:03:48 +0000

    Improved duration output

--- app/main.cpp
+++ app/main.cpp
@@ -2,6 +2,11 @@
 #include <gcode/Optimize.h>
 
 #include <iostream>
+#include <cmath>
+
+std::string format( double duration ){
+    return std::to_string( static_cast<long>(floor(duration)) ) + ":" + std::to_string( static_cast<long>((duration - floor(duration))*60.0) ) + "min";
+}
 
 int main( int argc, char** argv ){
 
@@ -22,8 +27,8 @@ int main( int argc, char** argv ){
         pos = endPosition( file[idx], pos );
     }
 
-    std::cout << "Estimated duration of original File: " << origDuration << std::endl;
-    std::cout << "Estimated duration of optimized File: " << file.duration() << std::endl;
+    std::cout << "Estimated duration of original File: " << format(origDuration) << std::endl;
+    std::cout << "Estimated duration of optimized File: " << format(file.duration()) << std::endl;
     std::cout << "Saving result to optimized.nc" << std::endl;
     file.save( "optimized.nc" );
 }
